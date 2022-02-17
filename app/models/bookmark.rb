1deeefb45e4a2eb436f51b99093ae9af1012dd6a
gary camboulives <gary.camboulives@gmail.com>
2022-02-17 12:22:32 +0100

    drtfgtryy

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { minmum: 6 }
-  validates :movie_id, uniqueness: {scope: :list_id }
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   validates :name, uniqueness: true
-  has_many :bookmarks, through: :movie
+  validates :name, presence: true
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
 end
