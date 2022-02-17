d030cb0cb78c2f60b1bda3d5ecd8505bc9af22b8
gary camboulives <gary.camboulives@gmail.com>
2022-02-17 17:35:37 +0100

    go front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+# url = "https://api.github.com/users/ssaunier"
+# user_serialized = URI.open(url).read
+# user = JSON.parse(user_serialized)
+
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+  movies = JSON.parse(URI.open(url).read)['results']
+  movies.each do |movie|
+    base_poster_url = 'https://image.tmdb.org/t/p/original'
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
