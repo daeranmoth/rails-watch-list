3276183631ca339b02af07739c12dee0c6299dcf
gary camboulives <gary.camboulives@gmail.com>
2022-02-18 09:23:51 +0100

    stimulus ok et front a finir

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,8 +17,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    bookmark = Bookmark.find(params[:id])
-    bookmark.destroy
+    @bookmark = Bookmark.find(params[:movie_id])
+    @bookmark.destroy
     redirect_to lists_path
   end
 
